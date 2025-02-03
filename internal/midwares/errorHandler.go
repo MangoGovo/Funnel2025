MangoGovo/GoServerBase

feat: 实现了服务的基本框架

internal/midwares/errorHandler.go
@@ -1,0 +1,51 @@
+package midwares
+
+import (
+	"errors"
+	"go-server-example/internal/apiException"
+	"go-server-example/internal/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+// ErrHandler 中间件用于处理请求错误。
+// 如果存在错误，将返回相应的 JSON 响应。
+func ErrHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 向下执行请求
+		c.Next()
+
+		// 如果存在错误，则处理错误
+		if len(c.Errors) > 0 {
+			err := c.Errors.Last().Err
+			if err != nil {
+				var apiErr *apiException.Error
+
+				// 尝试将错误转换为 apiException
+				ok := errors.As(err, &apiErr)
+
+				// 如果转换失败，则使用 ServerError
+				if !ok {
+					apiErr = apiException.ServerError
+					zap.L().Error("遇到了未知的异常:", zap.Error(err))
+				}
+
+				utils.JsonErrorResponse(c, apiErr.Code, apiErr.Msg)
+				return
+			}
+		}
+	}
+}
+
+// HandleNotFound 处理 404 错误。
+func HandleNotFound(c *gin.Context) {
+	err := apiException.NotFound
+	// 记录 404 错误日志
+	zap.L().Warn("404 Not Found",
+		zap.String("path", c.Request.URL.Path),
+		zap.String("method", c.Request.Method),
+	)
+	utils.JsonResponse(c, http.StatusNotFound, err.Code, err.Msg, nil)
+}

internal/utils/request/request.go
@@ -1,0 +1,56 @@
+//nolint:all
+package request
+
+// TODO 这里要好好看看
+import (
+	"crypto/tls"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	jsoniter "github.com/json-iterator/go"
+	"go.uber.org/zap"
+)
+
+// Client 包装 Resty 客户端
+type Client struct {
+	*resty.Client
+}
+
+// New 初始化一个 Resty 客户端
+func New() Client {
+	s := Client{
+		Client: resty.New().
+			SetTimeout(5 * time.Second).
+			SetJSONMarshaler(jsoniter.ConfigCompatibleWithStandardLibrary.Marshal).
+			SetJSONUnmarshaler(jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal),
+	}
+	// 利用中间件实现请求日志
+	s.OnAfterResponse(RestyLogMiddleware)
+	return s
+}
+
+// NewUnSafe 初始化一个 Resty 客户端并跳过 TLS 证书验证
+func NewUnSafe() Client {
+	s := New()
+	s.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	return s
+}
+
+// Request 获取一个新的请求实例
+func (s Client) Request() *resty.Request {
+	return s.R().EnableTrace()
+}
+
+// RestyLogMiddleware Resty日志中间件
+func RestyLogMiddleware(_ *resty.Client, resp *resty.Response) error {
+	if resp.IsError() {
+		method := resp.Request.Method
+		url := resp.Request.URL
+		zap.L().Warn("请求出现错误",
+			zap.String("method", method),
+			zap.String("url", url),
+			zap.Int64("time_spent(ms)", resp.Time().Milliseconds()),
+		)
+	}
+	return nil
+}

MangoGovo/Funnel2025

internal/midwares/errorHandler.go
@@ -1,0 +1,51 @@
+package midwares
+
+import (
+	"errors"
+	"funnel/internal/apiException"
+	"funnel/internal/utils/response"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+// ErrHandler 中间件用于处理请求错误。
+// 如果存在错误，将返回相应的 JSON 响应。
+func ErrHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 向下执行请求
+		c.Next()
+
+		// 如果存在错误，则处理错误
+		if len(c.Errors) > 0 {
+			err := c.Errors.Last().Err
+			if err != nil {
+				var apiErr *apiException.Error
+
+				// 尝试将错误转换为 apiException
+				ok := errors.As(err, &apiErr)
+
+				// 如果转换失败，则使用 ServerError
+				if !ok {
+					apiErr = apiException.ServerError
+					zap.L().Error("遇到了未知的异常:", zap.Error(err))
+				}
+
+				response.JsonError(c, apiErr.Code, apiErr.Msg)
+				return
+			}
+		}
+	}
+}
+
+// HandleNotFound 处理 404 错误。
+func HandleNotFound(c *gin.Context) {
+	err := apiException.NotFound
+	// 记录 404 错误日志
+	zap.L().Warn("404 Not Found",
+		zap.String("path", c.Request.URL.Path),
+		zap.String("method", c.Request.Method),
+	)
+	response.Json(c, http.StatusNotFound, err.Code, err.Msg, nil)
+}

pkg/request/request.go
@@ -1,0 +1,55 @@
+//nolint:all
+package request
+
+import (
+	"crypto/tls"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	jsoniter "github.com/json-iterator/go"
+	"go.uber.org/zap"
+)
+
+// Client 包装 Resty 客户端
+type Client struct {
+	*resty.Client
+}
+
+// New 初始化一个 Resty 客户端
+func New() Client {
+	s := Client{
+		Client: resty.New().
+			SetTimeout(5 * time.Second).
+			SetJSONMarshaler(jsoniter.ConfigCompatibleWithStandardLibrary.Marshal).
+			SetJSONUnmarshaler(jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal),
+	}
+	// 利用中间件实现请求日志
+	s.OnAfterResponse(RestyLogMiddleware)
+	return s
+}
+
+// NewWithoutTLS 初始化一个 Resty 客户端并跳过 TLS 证书验证
+func NewWithoutTLS() Client {
+	s := New()
+	s.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	return s
+}
+
+// Request 获取一个新的请求实例
+func (s Client) Request() *resty.Request {
+	return s.R().EnableTrace()
+}
+
+// RestyLogMiddleware Resty日志中间件
+func RestyLogMiddleware(_ *resty.Client, resp *resty.Response) error {
+	if resp.IsError() {
+		method := resp.Request.Method
+		url := resp.Request.URL
+		zap.L().Warn("请求出现错误",
+			zap.String("method", method),
+			zap.String("url", url),
+			zap.Int64("time_spent(ms)", resp.Time().Milliseconds()),
+		)
+	}
+	return nil
+}